d412a85c4127c518d890735cd9598eaa910fce56
jeon hyebeen <hyebin199953@gmail.com>
2023-06-30 09:39:11 +0900

    테스트 코드

--- src/main/java/com/main/MainProject/TestController.java
+++ src/main/java/com/main/MainProject/TestController.java
@@ -1,13 +1,17 @@
 package com.main.MainProject;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/test")
 public class TestController {
-    @GetMapping("/test")
-    public String getTest(){
-        return "server is work!!!";
+    @GetMapping
+    public ResponseEntity<String> getTest() {
+        return new ResponseEntity<>("server is working!!", HttpStatus.OK);
     }
 }
