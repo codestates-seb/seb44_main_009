9c5c4eeb76489b1f1b71a4a40398d4ab0686a0d5
hyuniiya <wngus7988.10@gmail.com>
2023-07-04 01:28:24 +0900

    feat:header layout

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,121 @@
+import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+import logo from "../../image/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faBasketShopping,
+} from "@fortawesome/free-solid-svg-icons";
+
+const HeaderContainer = styled.header`
+  position: relative;
+  width: 834px;
+  height: 100px;
+  border: 1px solid #383838;
+  border-radius: 30px 30px 0 0;
+  box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  -moz-box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    margin-left: 12px;
+    width: 90px;
+    height: 85px;
+    border-radius: 50%;
+  }
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid #383838;
+  border-radius: 30px;
+  width: 561px;
+  height: 45px;
+  padding-left: 12px;
+  margin-right: 24px;
+
+  input {
+    border: none;
+    outline: none;
+    width: 90%;
+    font-size: 16px;
+  }
+`;
+
+const SearchIconContainer = styled.div`
+  background-color: #383838;
+  border-radius: 30px;
+  width: 49px;
+  height: 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  color: #fff;
+  margin-right: 12px;
+  cursor: pointer;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const IconStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #383838;
+  color: #fff;
+  border-radius: 30px;
+  cursor: pointer;
+  font-size: 18px;
+  margin-right: 24px;
+  width: 45px;
+  height: 45px;
+`;
+
+const WriteStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #383838;
+  color: #fff;
+  border-radius: 30px;
+  cursor: pointer;
+  font-size: 12px;
+  margin-right: 24px;
+  width: 45px;
+  height: 45px;
+`;
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <img src={logo} alt="logo" />
+      </Link>
+      <InputContainer>
+        <input type="text" />
+        <SearchIconContainer>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </SearchIconContainer>
+      </InputContainer>
+      <ButtonContainer>
+        <IconStyle>
+          <FontAwesomeIcon icon={faBasketShopping} />
+        </IconStyle>
+        <Link to="/login">
+          <WriteStyle>로그인</WriteStyle>
+        </Link>
+      </ButtonContainer>
+    </HeaderContainer>
+  );
 }
 
 export default Header;
-
-// 빈 파일이 아니기 때문에 1치 머지 시, 충돌이 발생할 수 밖에 없음
-// 작업 시작 시, 해당 주석 삭제해주세요.
