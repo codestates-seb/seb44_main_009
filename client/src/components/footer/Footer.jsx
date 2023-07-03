9e18aab8f80b78761c184e7bf02d9f39f3ca634e
hyuniiya <wngus7988.10@gmail.com>
2023-07-04 02:27:18 +0900

    feat:footer layout

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,8 +1,57 @@
+import { styled } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faBars,
+  faHeart,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+const FooterContainer = styled.footer`
+  position: relative;
+  width: 834px;
+  height: 100px;
+  border: 1px solid #383838;
+  border-radius: 0 0 30px 30px;
+  box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  -moz-box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  justify-items: center;
+  align-items: center;
+`;
+
+const FooterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #383838;
+  color: #fff;
+  border-radius: 30px;
+  cursor: pointer;
+  font-size: 18px;
+  width: 45px;
+  height: 45px;
+`;
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <FontAwesomeIcon icon={faHouse} />
+      </FooterWrapper>
+      <FooterWrapper>
+        <FontAwesomeIcon icon={faBars} />
+      </FooterWrapper>
+      <FooterWrapper>
+        <FontAwesomeIcon icon={faHeart} />
+      </FooterWrapper>
+      <FooterWrapper>
+        <FontAwesomeIcon icon={faUser} />
+      </FooterWrapper>
+    </FooterContainer>
+  );
 }
 
 export default Footer;
-
-// 빈 파일이 아니기 때문에 1치 머지 시, 충돌이 발생할 수 밖에 없음
-// 작업 시작 시, 해당 주석 삭제해주세요.
