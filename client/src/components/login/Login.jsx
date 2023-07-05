9b394dc146f3968085c0058c311dacdfaf780a46
Xiel0112 <guswl000112@gmail.com>
2023-07-05 17:52:16 +0900

    fix(로그인 컴포넌트): 컴포넌트 경로 수정

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,6 +1,6 @@
-import Inputs from "./input/Inputs";
-import LinkBtn from "./button/LinkBtn";
-import LoginBtn from "./button/LoginBtn";
+import Inputs from "../user/sign/login/input/Inputs";
+import LinkBtn from "./LinkBtn";
+import LoginBtn from "./LoginBtn";
 import { LoginContainer } from "./styles/LoginContainer.styled";
 
 export default function Login({ children }) {
