6a57e4507e443756041d8fc292d495280af9b971
jeon hyebeen <hyebin199953@gmail.com>
2023-07-05 19:11:48 +0900

    feat/ merge bug fix

---
+++ server/MainProject/src/main/java/com/main/MainProject/temporary/AddressRepository.java
@@ -0,0 +1,6 @@
+package com.main.MainProject.temporary;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
