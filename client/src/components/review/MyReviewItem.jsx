8253cdf884d2310c804974aceba14afeab0f28bc
hyuniiya <wngus7988.10@gmail.com>
2023-07-07 15:01:59 +0900

    dir:myreview-page css분리

--- client/src/components/review/MyReviewItem.jsx
+++ client/src/components/review/MyReviewItem.jsx
@@ -3,42 +3,11 @@ import { ProductImage } from "../attribute/styles/ProdcutImage.styled";
 import { ProductDetail } from "../attribute/styles/ProductDetail.styled";
 import { ProductView } from "../attribute/styles/ProductView.styled";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { styled } from "styled-components";
-
-const ReviewItemContainer = styled.div`
-  border: 1px solid #383838;
-  border-radius: 20px;
-  padding: 24px;
-`;
-
-const StarIcon = styled(FontAwesomeIcon)`
-  margin-right: 5px;
-  font-size: 20px;
-  color: rgb(255, 81, 96);
-`;
-const ScoreText = styled.text`
-  font-size: 20px;
-`;
-const ButtonContainer = styled.div`
-  margin-top: 8px;
-`;
-
-const Button = styled.button`
-  margin-top: 12px;
-  width: 80px;
-  height: 30px;
-  background-color: #383838;
-  color: #fff;
-  border-radius: 20px;
-  font-size: 16px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: gray;
-    transition: 0.6s;
-  }
-`;
+import { ReviewItemContainer } from "./styles/myreview/ReviewItemContainer.styled";
+import { StarIcon } from "./styles/myreview/StarIcon.styled";
+import { ButtonWrapper } from "./styles/myreview/ButtonWrapper.styled";
+import { Button } from "./styles/myreview/Button.styled";
+import { Score } from "./styles/myreview/Score.styled";
 
 function MyReviewItem({ review }) {
   return (
@@ -49,12 +18,12 @@ function MyReviewItem({ review }) {
           <h2>{review.title}</h2>
           <p>{review.content}</p>
           <StarIcon icon={faStar} />
-          <ScoreText>{review.score}</ScoreText>
+          <Score>{review.score}</Score>
         </ProductView>
-        <ButtonContainer>
+        <ButtonWrapper>
           <Button>수정</Button>
           <Button>삭제</Button>
-        </ButtonContainer>
+        </ButtonWrapper>
       </ProductDetail>
     </ReviewItemContainer>
   );
