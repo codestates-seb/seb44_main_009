fd6a6ed8b0ea0b8e4428b024adbc5ef3d1160f13
Xiel0112 <guswl000112@gmail.com>
2023-07-10 15:27:52 +0900

    feat(회원가입 컴포넌트): 전화번호 유효성 메세지 기능 추가

--- client/src/components/user/singUp/userInfo/PhoneNumberInput.jsx
+++ client/src/components/user/singUp/userInfo/PhoneNumberInput.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SignUpInput } from "../styles/SignUpInput.styled";
 import { SignUpInputTitle } from "../styles/SignUpInputTitle.styled";
 import { ValidationMessage } from "../styles/ValidationMessage.styled";
@@ -6,13 +6,24 @@ import { SignUpInputTitleWrapper } from "../styles/SignUpInputTitleWrapper.style
 import { SingUpContext } from "../SignUp";
 
 export default function PhoneNumberInput() {
-  const { handleChange } = useContext(SingUpContext);
+  const { handleChange, signUpData } = useContext(SingUpContext);
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (signUpData.phoneNumber.length === 0) {
+      setMessage("전화번호를 입력해주세요");
+      return;
+    }
+
+    setMessage("");
+  }, [signUpData.phoneNumber]);
 
   return (
     <>
       <SignUpInputTitleWrapper>
         <SignUpInputTitle>전화번호</SignUpInputTitle>
-        <ValidationMessage>유효성 메세지</ValidationMessage>
+        <ValidationMessage>{message}</ValidationMessage>
       </SignUpInputTitleWrapper>
       <SignUpInput
         type="tel"
