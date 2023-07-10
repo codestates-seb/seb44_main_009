cc821e9be994a08513f953f4dbabca4bae2292ae
전혜빈 <80448017+Albatross53@users.noreply.github.com>
2023-07-10 10:27:46 +0900

    Delete server/server directory

--- server/server/MainProject/src/main/java/com/main/MainProject/cart/repository/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.MainProject.cart.repository;
-
-import com.main.MainProject.cart.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
