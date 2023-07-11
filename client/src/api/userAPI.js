58c783bf154fc82ce2f58b8725134ccdca003b3a
Xiel0112 <guswl000112@gmail.com>
2023-07-11 13:20:02 +0900

    feat(userAPI): 로그인 API 임시 구현

--- client/src/api/userAPI.js
+++ client/src/api/userAPI.js
@@ -6,3 +6,7 @@ axios.defaults.withCredentials = true;
 export const postSignUp = async data => {
   await axios.post("/", data);
 };
+
+export const postLogIn = async data => {
+  await axios.post("/", data);
+};
