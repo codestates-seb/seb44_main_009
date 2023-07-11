a06f28693f5c4186e9e30d568af7e0002f0631c8
Xiel0112 <guswl000112@gmail.com>
2023-07-11 14:20:29 +0900

    feat(로그인 컴포넌트): 로그인 요청 시, 유효성 검사에 따른 모달 띄우기

--- client/src/components/user/login/LoginBtn.jsx
+++ client/src/components/user/login/LoginBtn.jsx
@@ -1,5 +1,51 @@
+import { useContext } from "react";
 import { LoginButton } from "./styles/LoginButton.styled";
+import { LogInContext } from "./Login";
+import { postLogIn } from "../../../api/userAPI";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginBtn() {
-  return <LoginButton>로그인</LoginButton>;
+  // Context >> 사용
+  const { setShowModal, setValidation, emailRegEx, passworedRegEx, logInData } =
+    useContext(LogInContext);
+
+  // Navigate
+  const nav = useNavigate();
+
+  // handleEvent >> 유효성 검사 및 showModal(state) 변경
+  const handleOpenModal = () => {
+    for (let i in logInData) {
+      if (logInData[i].length === 0) {
+        setValidation("미입력한 부분이 없는지 확인해주세요.");
+        setShowModal(true);
+        return;
+      }
+    }
+
+    if (logInData.email.match(emailRegEx) === null) {
+      setValidation("형식에 맞춰 입력해주세요");
+      setShowModal(true);
+      return;
+    }
+
+    if (logInData.password.match(passworedRegEx) === null) {
+      setValidation("형식에 맞춰 입력해주세요");
+      setShowModal(true);
+      return;
+    }
+
+    // 유효성 검사 통과 시, api 요청
+    (async () => {
+      try {
+        await postLogIn(logInData);
+        setValidation("로그인에 성공하였습니다");
+        nav("/");
+      } catch (error) {
+        setValidation(error.message);
+      }
+    })();
+    setShowModal(true);
+  };
+
+  return <LoginButton onClick={handleOpenModal}>로그인</LoginButton>;
 }
