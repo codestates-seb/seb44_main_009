5f90c515ff1a536098296522387c6f3d3b09229c
Xiel0112 <guswl000112@gmail.com>
2023-07-12 17:34:17 +0900

    feat(마이페이지): 회원에 따른 회원 정보 변경

--- client/src/components/user/myPage/myPage/myInfo/MyInfo.jsx
+++ client/src/components/user/myPage/myPage/myInfo/MyInfo.jsx
@@ -1,10 +1,30 @@
+import { getUser } from "../../../../../api/userAPI";
 import MyInfoEditBtn from "./myInfoEditBtn/MyInfoEditBtn";
 import MyInfoImg from "./myInfoImg/MyInfoImg";
 import MyInformation from "./myInformation/MyInformation";
 import { MyInfoContainer } from "./styles/MyInfoContainer.styles";
+import { createContext, useEffect, useState } from "react";
+
+export const MyInfoContext = createContext();
 
 export default function MyInfo({ children }) {
-  return <MyInfoContainer>{children}</MyInfoContainer>;
+  // State >> API로 불러온 유저 정보
+  const [userInfo, setUserInfo] = useState();
+
+  console.log(userInfo);
+
+  // Effet >> API 요청으로 유저 정보 불러오기
+  useEffect(() => {
+    (async () => {
+      setUserInfo(await getUser());
+    })();
+  }, []);
+
+  return (
+    <MyInfoContext.Provider value={{ userInfo }}>
+      <MyInfoContainer>{children}</MyInfoContainer>
+    </MyInfoContext.Provider>
+  );
 }
 
 MyInfo.Img = MyInfoImg;
