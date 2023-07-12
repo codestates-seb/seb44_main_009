ec01c5cff7f8258ba0500e6ec49aab9d7aabfb6c
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2023-07-12 19:40:39 +0900

    fix 렌더링오류
    fix 렌더링오류

--- client/src/pages/main/ProductsPage/ProductsPage.jsx
+++ client/src/pages/main/ProductsPage/ProductsPage.jsx
@@ -2,14 +2,32 @@ import ProductList from "../../../components/mainProduct/ProductList";
 import { dummyproducts } from "../../../dummyDate/dummyProducts";
 import Header from "../../../components/header/Header";
 import Footer from "../../../components/footer/Footer";
+//import { useState } from "react";
 
 import { MainContainer, Borderdiv } from "./styles/ProductPageStyle";
 
 /* Filter 기능 추가 해야 함 ! */
-// 1. 각 카테고리와 ColorCategory 클릭 시, ProductsPage로 넘어가야하는 기능.
 // 2. 어떤 걸 눌렀느냐에 따라 Filter 되어 넘어가야 하는 기능.
 
 function ProductsPage() {
+  // const [products] = useState(dummyproducts);
+  // const [filteredProducts, setFilteredProducts] = useState(products);
+
+  // const handleFilterProducts = filter => {
+  //   if (filter === "Warm") {
+  //     const warmToneProducts = products.filter(
+  //       product => product.personalColor === "Warm",
+  //     );
+  //     setFilteredProducts(warmToneProducts);
+  //   } else if (filter === "Cool") {
+  //     const coolToneProducts = products.filter(
+  //       product => product.personalColor === "Cool",
+  //     );
+  //     setFilteredProducts(coolToneProducts);
+  //   } else {
+  //     setFilteredProducts(products);
+  //   }
+  // };
   return (
     <div>
       <MainContainer>
