0dda991a3d04face91a546d90610ac8b766bfe49
hyuniiya <wngus7988.10@gmail.com>
2023-07-17 16:59:55 +0900

    design: cartbadge css 분리

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -15,23 +15,7 @@ import {
   faMagnifyingGlass,
   faBasketShopping,
 } from "@fortawesome/free-solid-svg-icons";
-import { styled } from "styled-components";
-
-const CartBadge = styled.div`
-  position: absolute;
-  top: 20px;
-  right: 90px;
-  background-color: #ff5160;
-  color: white;
-  font-size: 12px;
-  font-weight: bold;
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+import { CartBadge } from "./styles/CartBadge.styled";
 
 function Header() {
   const products = useRecoilValue(productsState);
