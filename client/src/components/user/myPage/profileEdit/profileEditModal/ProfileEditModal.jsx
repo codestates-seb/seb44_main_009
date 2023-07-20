9eb78e5ab4496aa504881eb8355d05cabb1532b8
Xiel0112 <guswl000112@gmail.com>
2023-07-20 17:31:09 +0900

    feat(프로필 수정 페이지): 유효성 검사 추가

--- client/src/components/user/myPage/profileEdit/profileEditModal/ProfileEditModal.jsx
+++ client/src/components/user/myPage/profileEdit/profileEditModal/ProfileEditModal.jsx
@@ -1,12 +1,22 @@
+import { useContext } from "react";
 import { ProfileEditModalBtn } from "./styles/ProfileEditModalBtn.styled";
 import { ProfileEditModalContainer } from "./styles/ProfileEditModalContainer.styled";
 import { ProfileEditModalMessage } from "./styles/ProfileEditModalMessage.styled";
+import { MyProfileEditsContext } from "../MyProfileEdit";
 
 export default function ProfileEditModal() {
+  // Context >> 사용
+  const { setShowModal, validation } = useContext(MyProfileEditsContext);
+
+  // handleEvent >> showModal(state) 변경
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <ProfileEditModalContainer>
-      <ProfileEditModalMessage>메세지</ProfileEditModalMessage>
-      <ProfileEditModalBtn>닫기</ProfileEditModalBtn>
+      <ProfileEditModalMessage>{validation}</ProfileEditModalMessage>
+      <ProfileEditModalBtn onClick={handleCloseModal}>닫기</ProfileEditModalBtn>
     </ProfileEditModalContainer>
   );
 }
