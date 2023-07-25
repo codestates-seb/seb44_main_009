c743d0c16543b3be1e337e010dedd569566f6608
hyuniiya <wngus7988.10@gmail.com>
2023-07-25 12:52:37 +0900

    design:Modaldev css분리

---
+++ client/src/components/attribute/Modaldev/styles/ModalTitle.jsx
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+export const ModalTitle = styled.h2`
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
---
+++ client/src/components/attribute/Modaldev/styles/ModaldevStyles.js
@@ -0,0 +1,17 @@
+import { ModalBackdrop } from "./ModalBackdrop";
+import { ModalContent } from "./ModalContent";
+import { ModalTitle } from "./ModalTitle";
+import { MessageContainer } from "./MessageContainer";
+import { Message } from "./Message";
+import { Icon } from "./Icon";
+import { CloseButton } from "./CloseButton";
+
+export {
+  ModalBackdrop,
+  ModalContent,
+  ModalTitle,
+  MessageContainer,
+  Message,
+  Icon,
+  CloseButton,
+};
