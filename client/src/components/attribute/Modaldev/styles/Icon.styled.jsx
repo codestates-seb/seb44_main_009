21837a6ca9051c0a7062f8a1cf25029451679d9f
hyuniiya <wngus7988.10@gmail.com>
2023-07-25 14:25:56 +0900

    design:Modaldev css분리

---
+++ client/src/components/attribute/Modaldev/styles/Icon.styled.jsx
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Icon = styled(FontAwesomeIcon)`
+  margin-right: 10px;
+  font-size: 22px;
+`;
---
+++ client/src/components/attribute/Modaldev/styles/ModalBackdrop.styled.jsx
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const ModalBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 5;
+`;
