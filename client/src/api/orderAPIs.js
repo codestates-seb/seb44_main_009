fef7de32737b01f3054eac6b0880a0defcb3db1d
hyuniiya <wngus7988.10@gmail.com>
2023-07-26 18:02:05 +0900

    fix:url

--- client/src/api/orderAPIs.js
+++ client/src/api/orderAPIs.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://3c46-119-206-216-76.ngrok-free.app/";
+axios.defaults.baseURL =
+  "http://ec2-43-201-65-189.ap-northeast-2.compute.amazonaws.com:8080";
 
 // 장바구니 전체 조회
 export const fetchCart = async token => {
--- client/src/components/footer/Footer_doubleBtn.jsx
+++ client/src/components/footer/Footer_doubleBtn.jsx
@@ -10,7 +10,7 @@ import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { BuyFooterModal } from "./BuyFooterModal";
 import { useRecoilValue } from "recoil";
 import { productsState } from "../../atoms/product";
-import { updateLike, deleteLike } from "../../api/orderAPIs";
+import { updateLike, deleteLike, fetchLike } from "../../api/orderAPIs";
 import { auth } from "../../atoms/auth";
 import { styled } from "styled-components";
 
@@ -79,7 +79,19 @@ function Footer_doubleBtn() {
       console.error(error);
     }
   };
+  // 찜 전체 조회
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchLike(token);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
+    fetchData();
+  }, []);
   // const handleClick = () => {
   //   if (!isLiked) {
   //     setCount(prevCount => prevCount + 1);
